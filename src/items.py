026b56194e758093dcb1258ac8b19a428df5d196
nick.durbin <nick.durbin@gmail.com>
2020-06-09 13:26:48 -0400

    Created the initial items class.

---
+++ src/items.py
@@ -0,0 +1,17 @@
+# A class for the items in the rooms.
+import player from Player
+
+class Items:
+  def __init__(self, name, description, level):
+    self.name = name
+    self.description = description
+    self.level = int(level)
+
+  def picked_up(self):
+    print(f"Behold, {player.name}. You have discovered the {self.name} a level{self.level}.\nThe{self.name} is {self.description}.")
+
+  def put_down(self):
+    print(f"Yeah, who needs a {self.name} anyway? It was only saving your life.")
+
+  def __repr__(self):
+    return f"self.name = {self.name} ; self.description = {self.description} ; self/level = {self.level}"
