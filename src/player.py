bfeb8c327af6a30a5ee89445aa7b35617c283748
nick.durbin <nick.durbin@gmail.com>
2020-06-09 13:49:28 -0400

    Adding to the game. Need to scaffold more.

--- src/player.py
+++ src/player.py
@@ -2,21 +2,33 @@
 # currently.
 
 class Player:
-  def __init__(self, name, actions, items, location):
+  def __init__(self, name, items=[], location):
     self.name = name
-    self.actions = actions
     self.items = items
     self.location = location
+
+    def player_actions(self, input):
+      if input == "w":
+        direction = "north"
+      if input == "a":
+        direction = "west"
+      if input == "s":
+        direction == "south"
+      if input == "d":
+        direction == "east"
+
+      print(f"You have chosen to go {input.direction}.")
 
     def __str__(self):
       return None
 
     def __repr__(self):
-      return f"self.name = {self.name}; self.actions = {self.actions}; self.items = {self.items}; self.items = {self.items}"
+      return f"self.name = {self.name}; self.items = {self.items}; self.items = {self.items}"
+
 
 class Sub_Class(Player):
   def __init__(self, name, description, abilities, level):
-    super().__init__(name, actions, items, location)
+    super().__init__(name, items, location)
       self.name = name
       self.description = description
       self.abilities = abilities
